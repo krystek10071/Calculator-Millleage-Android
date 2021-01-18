d11621a5f1f39b8651866ba1d8b5b4b97c1daffc
Krisu1007 <krystek10071@wp.pl>
2021-01-18 11:57:40 +0100

    add fragment statistics

---
+++ app/src/main/java/com/example/finalprojectandroid/statisticComsumptionFuel/FragmentActivity/StatisticGeneralFragment.java
@@ -0,0 +1,43 @@
+package com.example.finalprojectandroid.statisticComsumptionFuel.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.finalprojectandroid.R;
+
+public class StatisticGeneralFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.general_statistic_fragment, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
