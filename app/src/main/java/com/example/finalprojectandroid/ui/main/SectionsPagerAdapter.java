3469fe44241e942d4f5797b7ebe3e0597498d54b
Krisu1007 <krystek10071@wp.pl>
2021-01-19 12:09:07 +0100

    add fragment 1 layout

--- app/src/main/java/com/example/finalprojectandroid/ui/main/SectionsPagerAdapter.java
+++ app/src/main/java/com/example/finalprojectandroid/ui/main/SectionsPagerAdapter.java
@@ -9,6 +9,11 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 import com.example.finalprojectandroid.R;
+import com.example.finalprojectandroid.calculateConsumptionFuel.activity.Fragment1;
+import com.example.finalprojectandroid.calculateConsumptionFuel.activity.Fragment2;
+import com.example.finalprojectandroid.calculateConsumptionFuel.activity.Fragment3;
+
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -17,7 +22,7 @@ import com.example.finalprojectandroid.R;
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @StringRes
-    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
     private final Context mContext;
 
     public SectionsPagerAdapter(Context context, FragmentManager fm) {
@@ -25,11 +30,29 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
         mContext = context;
     }
 
+    @NotNull
     @Override
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        Fragment fragment = null;
+
+        switch (position){
+            case 0:
+                fragment = new Fragment1();
+                break;
+            case 1:
+                fragment = new Fragment2();
+                break;
+            case 2:
+                fragment = new Fragment3();
+                break;
+        }
+
+        if (fragment != null) {
+            return fragment;
+        }
+        return new Fragment1();
     }
 
     @Nullable
@@ -40,7 +63,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        // Show 3 total pages.
+        return 3;
     }
 }
